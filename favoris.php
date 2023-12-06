ef6bb7a562df8ab354b6ed166833fab4712fa06e
Hicham Abekiri <itshichabk@gmail.com>
2023-12-06 10:04:25 -0500

    Ajout classe Televiseur + TeleviseurManager

--- favoris.php
+++ favoris.php
@@ -1,18 +1,24 @@
 <?php
     require_once 'class/PDOFactory.php';
+    require_once 'class/TeleviseurManager.php';
+    include_once 'inc/header.php';
+
     $bdd = PDOFactory::getMySQLConnection();
 
-    $bddResults = $bdd->query("SELECT marque.nom, televiseur.nom, modele, prix FROM televiseur JOIN marque ON marque.id = televiseur.id_marque;");
-    $data = $bddResults->fetchAll();
+    //$bddResults = $bdd->query("SELECT marque.nom, televiseur.nom, modele, prix FROM televiseur JOIN marque ON marque.id = televiseur.id_marque;");
+    //$data = $bddResults->fetchAll();
 
-    include_once 'inc/header.php';
+    $tvMgr = new TeleviseurManager($bdd);
+    $tvs = $tvMgr->getTeleviseurs();
+    
 ?>
 
+<main>
 <div id="favoris">
     <?php
         
     ?>
-    <div class="favori">
+    <!--div class="favori">
         <div class="favori-row">
             <img src="https://i5.walmartimages.ca/images/Enlarge/439/687/6000206439687.jpg" alt="">
             <div class="favori-details">
@@ -104,8 +110,9 @@
             <a href="">Consulter</a>
             <a href="">Supprimer</a>
         </div>
-    </div>
+    </div-->
 
 </div>
+</main>
 
 <?php include_once 'inc/footer.php'; ?>
